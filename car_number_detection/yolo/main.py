16532f6d259e469a1a166ec41ae85f7dee70305d
Panich Maxim <panichmax@gmail.com>
2021-08-19 19:16:34 +0300

    Refactor

--- car_number_detection/yolo/main.py
+++ car_number_detection/yolo/main.py
@@ -6,38 +6,7 @@ import sys
 import numpy as np
 import os.path
 
-# Initialize the parameters
-confThreshold = 0.5  # Confidence threshold
-nmsThreshold = 0.4  # Non-maximum suppression threshold
-
-inpWidth = 416  # 608     # Width of network's input image
-inpHeight = 416  # 608     # Height of network's input image
-
-parser = argparse.ArgumentParser(
-    description='Object Detection using YOLO in OPENCV')
-parser.add_argument('--inputFilePath', help='Path to image file.')
-parser.add_argument('--modelPath', help='Path to image file.')
-args = parser.parse_args()
-
-# Load names of classes
-classesFile = "classes.names"
-
-classes = None
-with open(classesFile, 'rt') as f:
-    classes = f.read().rstrip('\n').split('\n')
-
-# Give the configuration and weight files for the model and load the network using them.
-
-modelConfiguration = "darknet-yolov3.cfg"
-modelWeights = args.modelPath
-
-net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
-
 # Get the names of the output layers
-
-
 def getOutputsNames(net):
     # Get the names of all the layers in the network
     layersNames = net.getLayerNames()
@@ -45,8 +14,6 @@ def getOutputsNames(net):
     return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 # Draw the predicted bounding box
-
-
 def drawPred(classId, conf, left, top, right, bottom):
     # Draw a bounding box.
     #    cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
@@ -66,12 +33,9 @@ def drawPred(classId, conf, left, top, right, bottom):
     cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(
         1.5*labelSize[0]), top + baseLine), (255, 0, 255), cv.FILLED)
     #cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine),    (255, 255, 255), cv.FILLED)
-    cv.putText(frame, label, (left, top),
-               cv.FONT_HERSHEY_SIMPLEX, 0.70, (255, 255, 255), 2)
+    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.70, (255, 255, 255), 2)
 
 # Remove the bounding boxes with low confidence using non-maxima suppression
-
-
 def postprocess(frame, outs):
     frameHeight = frame.shape[0]
     frameWidth = frame.shape[1]
@@ -120,51 +84,76 @@ def postprocess(frame, outs):
         drawPred(classIds[i], confidences[i], left,
                  top, left + width, top + height)
 
+if __name__ == "__main__":
+    # Initialize the parameters
+    confThreshold = 0.5  # Confidence threshold
+    nmsThreshold = 0.4  # Non-maximum suppression threshold
 
-# Process inputs
-winName = 'Deep learning object detection in OpenCV'
-cv.namedWindow(winName, cv.WINDOW_NORMAL)
+    inpWidth = 416  # 608     # Width of network's input image
+    inpHeight = 416  # 608     # Height of network's input image
 
-outputFile = "result.jpg"
-if (args.inputFilePath):
-    # Open the image file
-    if not os.path.isfile(args.inputFilePath):
-        print("Input image file ", args.inputFilePath, " doesn't exist")
-        sys.exit(1)
-    cap = cv.VideoCapture(args.image)
+    parser = argparse.ArgumentParser(description='Object Detection using YOLO in OPENCV')
+    parser.add_argument('--inputFilePath', help='Path to image file.')
+    parser.add_argument('--modelPath', help='Path to image file.')
+    args = parser.parse_args()
 
-while cv.waitKey(1) < 0:
+    # Load names of classes
+    classesFile = "classes.names"
+    classes = None
+    with open(classesFile, 'rt') as f:
+        classes = f.read().rstrip('\n').split('\n')
 
-    # get frame from the video
-    hasFrame, frame = cap.read()
+    # Give the configuration and weight files for the model and load the network using them.
+    modelConfiguration = "darknet-yolov3.cfg"
+    modelWeights = args.modelPath
 
-    # Stop the program if reached end of video
-    if not hasFrame:
-        print("Done processing !!!")
-        print("Output file is stored as ", outputFile)
-        cv.waitKey(3000)
-        break
+    net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+    net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+    net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
-    # Create a 4D blob from a frame.
-    blob = cv.dnn.blobFromImage(
-        frame, 1/255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+    # Process inputs
+    winName = 'Deep learning object detection in OpenCV'
+    cv.namedWindow(winName, cv.WINDOW_NORMAL)
 
-    # Sets the input to the network
-    net.setInput(blob)
-
-    # Runs the forward pass to get output of the output layers
-    outs = net.forward(getOutputsNames(net))
-
-    # Remove the bounding boxes with low confidence
-    postprocess(frame, outs)
-
-    # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-    t, _ = net.getPerfProfile()
-    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
-    #cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-
-    # Write the frame with the detection boxes
+    outputFile = "result.jpg"
     if (args.inputFilePath):
-        cv.imwrite(outputFile, frame.astype(np.uint8))
-    else:
-        vid_writer.write(frame.astype(np.uint8))
+        # Open the image file
+        if not os.path.isfile(args.inputFilePath):
+            print("Input image file ", args.inputFilePath, " doesn't exist")
+            sys.exit(1)
+        cap = cv.VideoCapture(args.image)
+
+    while cv.waitKey(1) < 0:
+
+        # get frame from the video
+        hasFrame, frame = cap.read()
+
+        # Stop the program if reached end of video
+        if not hasFrame:
+            print("Done processing !!!")
+            print("Output file is stored as ", outputFile)
+            cv.waitKey(3000)
+            break
+
+        # Create a 4D blob from a frame.
+        blob = cv.dnn.blobFromImage(frame, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+
+        # Sets the input to the network
+        net.setInput(blob)
+
+        # Runs the forward pass to get output of the output layers
+        outs = net.forward(getOutputsNames(net))
+
+        # Remove the bounding boxes with low confidence
+        postprocess(frame, outs)
+
+        # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
+        t, _ = net.getPerfProfile()
+        label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+        # cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+        # Write the frame with the detection boxes
+        if (args.inputFilePath):
+            cv.imwrite(outputFile, frame.astype(np.uint8))
+
+
